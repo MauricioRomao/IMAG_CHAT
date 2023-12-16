a77bd2430ccf6147fef085c1bdcf7aa76f79a4d3
Joshua_DaCosta_ <joshuagcdacosta@gmail.com>
2023-12-16 18:08:14 +0100

    adaptando o backend com o frontend

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template,redirect,request,flash
 
-
-app= Flask(__name__)
+app= Flask(__name__, template_folder="../frontend/App/templates")
 app.config['SECRET_KEY']="IMAGSEGURA"
-from app.controllers import rotas
+from backend.controllers import rotas
--- backend/controllers/rotas.py
+++ backend/controllers/rotas.py
@@ -1,4 +1,4 @@
-from app import *
+from backend import *
 from flask import Flask, render_template,redirect,request,flash,session,abort,url_for
 import json
 import requests as requests
@@ -8,7 +8,7 @@ import requests as requests
 @app.route('/home')
 @app.route('/')
 def home():
-    return 'chat está no ar'
+    return render_template('index.html')
 @app.route("/criar")
 def criar():
     return "pagina de criar login"
